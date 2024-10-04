765a1dfeea8cc02a4faf447d413f7ce3334abd23
John Colton <jcolton96@gmail.com>
2024-10-04 15:58:48 +0100

    structs lesson refactor

--- structs_methods_interfaces/shapes.go
+++ structs_methods_interfaces/shapes.go
@@ -2,6 +2,10 @@ package shapes
 
 import "math"
 
+type Shape interface {
+	Area() float64
+}
+
 type Rectangle struct {
 	Width  float64
 	Height float64
@@ -22,4 +26,13 @@ type Circle struct {
 
 func (c Circle) Area() float64 {
 	return math.Pi * math.Pow(c.Radius, 2)
+}
+
+type Triangle struct {
+	Base   float64
+	Height float64
+}
+
+func (t Triangle) Area() float64 {
+	return 0.5 * (t.Base * t.Height)
 }
--- structs_methods_interfaces/shapes_test.go
+++ structs_methods_interfaces/shapes_test.go
@@ -13,22 +13,19 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	t.Run("rectangles", func(t *testing.T) {
-		rectangle := Rectangle{12.0, 6.0}
-		got := rectangle.Area()
-		want := 72.0
+	areaTests := []struct {
+		shape   Shape
+		hasArea float64
+	}{
+		{shape: Rectangle{12, 6}, hasArea: 72.0},
+		{shape: Circle{10}, hasArea: 314.1592653589793},
+		{shape: Triangle{12, 6}, hasArea: 36.0},
+	}
 
-		if got != want {
-			t.Errorf("got %g want %g", got, want)
+	for _, tt := range areaTests {
+		got := tt.shape.Area()
+		if got != tt.hasArea {
+			t.Errorf("%#v got %g want %g", tt.shape, got, tt.hasArea)
 		}
-	})
-	t.Run("circles", func(t *testing.T) {
-		circle := Circle{10.0}
-		got := circle.Area()
-		want := 314.1592653589793
-
-		if got != want {
-			t.Errorf("got %g want %g", got, want)
-		}
-	})
+	}
 }
